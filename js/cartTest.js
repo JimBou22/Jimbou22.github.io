ac0c2d1a0d1234ae2b8c908eeb3088b6eed92e25
James Bou <Jbou422@gmail.com>
2020-04-14 14:57:33 -0700

    some jank html fixes

--- js/cartTest.js
+++ js/cartTest.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     var name = $(this).data("name");
     var price = $(this).data("price");
     var quantity = $("#qty-1").attr("value");
-    alert(name + price + quantity + " is the product");
+    // alert(name + price + quantity + " is the product");
     var x = new cartItem(name,quantity,price);
     // if(sessionStorage.getItem("myCart") ===undefined){
     //   var cart = {x};
@@ -21,24 +21,35 @@ $(document).ready(function(){
     //   sessionStorage.setItem("myCart", JSON.stringify(myStuff))
     // }
     sessionStorage.setItem(name,name);
-    sessionStorage.setItem("3DPrinterQuantity",quantity);
-    sessionStorage.setItem("3DPrinterPrice",price);
-
+    sessionStorage.setItem(name + "Quantity",quantity);
+    sessionStorage.setItem(name + "Price",price);
     });
 
-    
+var keys = Object.keys(sessionStorage);
+for(var i =0; i<keys.length;i+=3){
+  $("#n" + (i/3+1)).html(sessionStorage.getItem(keys[i]));
+  $("#q" + (i/3+1)).html(sessionStorage.getItem(keys[i + 2]));
+  $("#p" + (i/3+1)).html(sessionStorage.getItem(keys[i + 1]));
+}
 
-  $("#n1").html(sessionStorage.getItem("3DPrinter"));
-  $("#q1").html(sessionStorage.getItem("3DPrinterQuantity"));
-  $("#p1").html(sessionStorage.getItem("3DPrinterPrice"));
+// alert("right before 2nd for loop");
+for(var j = 1; j <=3; j++){
+  var sub = parseFloat(document.getElementById("p"+ j).innerHTML.replace("$",""));
+  alert(sub);
+  document.getElementById("sub-total").innerHTML += sub;
+}
 
-//
-// function display() {
-//     var cart = JSON.parse(sessionStorage.getItem("myCart"));
-//     var content = document.getElementById("cartTable");
-//
-//     content.innerHTML = cart.map(product => {
-//         return '<tr><td>' + product.name + '</td><td>' + product.price + '</td></tr>';
-//     }).join('');
-// }
+  // $("#n2").html(sessionStorage.getItem("3DPrinter"));
+  // $("#q2").html(sessionStorage.getItem("3DPrinterQuantity"));
+  // $("#p2").html(sessionStorage.getItem("3DPrinterPrice"));
+  // $("#n3").html(sessionStorage.getItem("3DPrinter"));
+  // $("#q3").html(sessionStorage.getItem("3DPrinterQuantity"));
+  // $("#p3").html(sessionStorage.getItem("3DPrinterPrice"));
+  // $("#n4").html(sessionStorage.getItem("3DPrinter"));
+  // $("#q4").html(sessionStorage.getItem("3DPrinterQuantity"));
+  // $("#p4").html(sessionStorage.getItem("3DPrinterPrice"));
+  // $("#n5").html(sessionStorage.getItem("3DPrinter"));
+  // $("#q5").html(sessionStorage.getItem("3DPrinterQuantity"));
+  // $("#p5").html(sessionStorage.getItem("3DPrinterPrice"));
+
 });
