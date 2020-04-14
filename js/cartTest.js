a6b86b12878234a09d88ccd41b6483621811a4fd
James Bou <Jbou422@gmail.com>
2020-04-14 10:59:12 -0700

    Cart

--- js/cartTest.js
+++ js/cartTest.js
@@ -1,12 +1,44 @@
-var cart = [];
+$(document).ready(function(){
+  class cartItem {
+    constructor(name, qty, price){
+      this.name=name;
+      this.qty=qty;
+      this.price=price;
+    }
+  }
+  $("#cart").click(function(){
+    var name = $(this).data("name");
+    var price = $(this).data("price");
+    var quantity = $("#qty-1").attr("value");
+    alert(name + price + quantity + " is the product");
+    var x = new cartItem(name,quantity,price);
+    // if(sessionStorage.getItem("myCart") ===undefined){
+    //   var cart = {x};
+    //   sessionStorage.setItem("myCart",JSON.stringify(cart));
+    // } else {
+    //   var myStuff = JSON.parse(sessionStorage.getItem("myCart"));
+    //   myStuff.push(x);
+    //   sessionStorage.setItem("myCart", JSON.stringify(myStuff))
+    // }
+    sessionStorage.setItem(name,name);
+    sessionStorage.setItem("3DPrinterQuantity",quantity);
+    sessionStorage.setItem("3DPrinterPrice",price);
 
-var Item = function(name,price,quantity){
-  this.name = name;
-  this.price = price;
-  this.quantity = quantity;
-}
+    });
 
-function addItemToCart(name,price,quantity){
-  var item = new Item(name,price,quantity);
-  cart.push(item);
-}
+
+
+  $("#n1").html(sessionStorage.getItem("3DPrinter"));
+  $("#q1").html(sessionStorage.getItem("3DPrinterQuantity"));
+  $("#p1").html(sessionStorage.getItem("3DPrinterPrice"));
+
+//
+// function display() {
+//     var cart = JSON.parse(sessionStorage.getItem("myCart"));
+//     var content = document.getElementById("cartTable");
+//
+//     content.innerHTML = cart.map(product => {
+//         return '<tr><td>' + product.name + '</td><td>' + product.price + '</td></tr>';
+//     }).join('');
+// }
+});
